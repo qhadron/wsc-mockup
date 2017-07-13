b67db0485785169e5742443be3a453fca0a28726
qhadron <jack.lee2980@gmail.com>
2017-07-13 21:05:30 +0000

    Make sure updates aren't triggered while a previous one is being triggered

--- custom-elems.js
+++ custom-elems.js
@@ -262,7 +262,14 @@ let populateHistoryTabs  = (function () {
 	// wait a bit and initialize tabs. Delay on first trigger must be at least 1s to account for $ initialization
 	.map(e =>  {
 		this.first = typeof this.first == "undefined";
-		waitabit(this.first ? 1000 : 0).then(_ => $(e).trigger('wb-init.wb-tabs'))
+		if (!this.waiting){
+			console.log("Updating history tab, first is ", this.first);
+			this.waiting = true;
+			waitabit(this.first ? 1000 : 0).then(_ => {
+				$(e).trigger('wb-init.wb-tabs');
+				this.waiting=false;
+			});
+		}
 	});
 }).bind({});
 
